22b2bd12cbaf1bbf1c7683ae4f3b9f736b4ba5be
hdavidlp <heclop@hotmail.com>
2022-10-12 11:23:20 -0700

    Feedback form and rating select include

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 
 import React from "react"
+import {v4 as uuidv4} from 'uuid'
 import {useState}  from "react"
 import Header from "./components/Header"
 import FeedbackList from "./components/FeedbackList"
 import FeedbackStats from "./components/FeedbackStats"
+import FeedbackForm from "./components/FeedbackForm"
 //import FeedbackItem from "./components/FeedbackItem"
 import FeedbackData from "./data/FeedbackData"
 
@@ -20,13 +22,21 @@ function App() {
         if (window.confirm('Are you sure?')){
             setFeedback(feedback.filter((item) => item.id!== id))
         }
-
     }
+
+    const addFeedBack=(newFeedBack)=>{
+        newFeedBack.id = uuidv4()
+        //console.log(newFeedBack);
+        setFeedback([newFeedBack, ...feedback])
+    }
+
+
 
     return (
         <>
             <Header  />
             <div className="container">
+                <FeedbackForm handleAdd={addFeedBack}/>
                 <FeedbackStats feedback={feedback} />
                 <FeedbackList feedback={feedback}
                 handleDelete = {deleteFeedBack}
